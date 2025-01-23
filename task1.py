a60f857a6f792da6d86ae8c7554ba8b64a083d10
Maxim <kmaxusk@gmail.com>
2025-01-23 11:56:06 +0200

    task1

--- task1.py
+++ task1.py
@@ -1,18 +1,29 @@
-import sys
+import sys, shutil
 from pathlib import Path
 
-def display_tree(path: Path, indent: str = "") -> None:
-    print(indent + str(path.name))
+def search_tree(path: Path) -> None:    
 
     if path.is_dir():
         for child in path.iterdir():
-            display_tree(child, indent + "    ")
+            search_tree(child)
+    else:
+        copy_file(dir_target, path)       
 
+def copy_file(path: Path, file_name: Path):
+    
+    if not path.exists():
+        path.mkdir(parents=False, exist_ok=True)
+    
+    [_, ext]= str(file_name.name).split('.')
+    dir_to_copy = Path(ext)
+    directory = Path(str(path.name) + '/' + ext)    
+    
+    if dir_to_copy not in path.iterdir():    
+        directory.mkdir(parents=False, exist_ok=True)   
+    
+    shutil.copy(file_name, directory)
 
-dir_source = sys.argv[1]
-dir_target = sys.argv[2] if len(sys.argv) > 2  else "dist"
+dir_source = Path(sys.argv[1])
+dir_target = Path(sys.argv[2]) if len(sys.argv) > 2  else Path("dist")
 
-
-
-print(dir_source)
-print(dir_target)
+search_tree(dir_source)
